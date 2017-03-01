92990165105a6c7d9b4166e46a582d6e024a86b9
Ay Beskyd <beskydmail@gmail.com>
2017-03-01 22:10:47 +0000

    add script.js

---
+++ An-Advanced-Guide/script.js
@@ -0,0 +1,10 @@
+var desk = new Vue({
+  el: "#desk",
+  data: {
+    showDesk: true,
+    screenLogo: "http://apple-wholesale-store.com/wp-  content/uploads/2017/01/Apple_Logo_Solid_White-1.png"
+  },
+  methods: {
+    
+  }
+});
