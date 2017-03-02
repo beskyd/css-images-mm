bcc1f8654c1c1aedeb5c38783ac9e53062bf4c32
Ay Beskyd <beskydmail@gmail.com>
2017-03-02 21:49:32 +0000

    add vue.js script to switch between the scenes

--- An-Advanced-Guide/script.js
+++ An-Advanced-Guide/script.js
@@ -2,9 +2,31 @@ var desk = new Vue({
   el: "#desk",
   data: {
     showDesk: true,
-    screenLogo: "http://apple-wholesale-store.com/wp-  content/uploads/2017/01/Apple_Logo_Solid_White-1.png"
+    screenLogo: "http://apple-wholesale-store.com/wp-content/uploads/2017/01/Apple_Logo_Solid_White-1.png"
   },
   methods: {
-    
+    processSticky: function(selection) {
+      if (selection === 1) {
+        // same but for red sticky scene
+      } else if (selection === 2) {
+        this.showDesk = false;
+        stickyYellow.showStickyYellow = true
+      }
+    }
+  }
+});
+
+
+var stickyYellow = new Vue({
+  el: "#stickyYellow",
+  data: {
+    showStickyYellow: false,
+    stickyText: ''
+  },
+  methods: {
+    processText: function() {
+      this.showStickyYellow = false;
+      desk.showDesk = true;
+    }
   }
 });
